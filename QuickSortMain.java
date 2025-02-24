c4a80acbd112c8eef5f640e33526b523a14e7d68
Yovo Mihov <yovomihov05@gmail.com>
2025-02-24 13:17:57 +0200

    Add files via upload

--- QuickSortMain.java
+++ QuickSortMain.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Random;
 
 public class QuickSortMain {
     public static void main(String[] args) throws Exception {
@@ -9,10 +10,10 @@ public class QuickSortMain {
             int N = Integer.parseInt(reader.readLine());
 
             int[] array = new int[N];
+            Random random = new Random();
 
-            System.out.println("Въведете " + N + " числа :");
             for (int i = 0; i < N; i++) {
-                array[i] = Integer.parseInt(reader.readLine());
+                 array[i] = random.nextInt(100);
             }
 
             System.out.println("Не сортиран масив: " + Arrays.toString(array));
